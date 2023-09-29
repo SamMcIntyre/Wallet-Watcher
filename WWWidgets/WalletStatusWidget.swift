7707ae1672d9ab064435a3e8f6303ff46a63bff7
Samuel McIntyre <sammac1229@live.com>
2023-09-28 19:58:34 -0500

    Working widgets
    got wallet status widget working

--- WWWidgets/WalletStatusWidget.swift
+++ WWWidgets/WalletStatusWidget.swift
@@ -21,27 +21,30 @@ struct StatusProvider: TimelineProvider {
 		}
 	}
 	
+	//basic placeholder
     func placeholder(in context: Context) -> StatusEntry {
 		StatusEntry(wallet: Wallet(spent: 30.00, budget: 200.00))
     }
 
+	//for a quick snapshot
 	@MainActor
 	func getSnapshot(in context: Context, completion: @escaping (StatusEntry) -> ()) {
 		
 		//modelContainer.mainContext.fetch(fetchDescriptor)
 		let wallets = try? modelContainer.mainContext.fetch(FetchDescriptor<Wallet>())
 		
-		let entry = StatusEntry(wallet: wallets?[0] ?? Wallet(spent: 30.00, budget: 200.00))
+		let entry = StatusEntry(wallet: wallets?[0] ?? Wallet(spent: -1.00, budget: 200.00))
         completion(entry)
     }
 
+	//for a timeline (basically a snapshot in this instance)
 	@MainActor
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [StatusEntry] = []
 		
 		let wallets = try? modelContainer.mainContext.fetch(FetchDescriptor<Wallet>())
 
-		let entry = StatusEntry(wallet: wallets?[0] ?? Wallet(spent: 30.00, budget: 200.00))
+		let entry = StatusEntry(wallet: wallets?[0] ?? Wallet(spent: -1.00, budget: 200.00))
 		entries.append(entry)
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
@@ -55,14 +58,35 @@ struct StatusEntry: TimelineEntry {
 }
 
 struct StatusEntryView : View {
-	//@Environment(\.modelContext) private var modelContext
-	//@Query private var wallets: [Wallet]
-	
     var entry: StatusProvider.Entry
 
+	@Environment(\.widgetFamily) var family
+	
     var body: some View {
         VStack {
-			Text(String(entry.wallet.spent))
+			let spent = entry.wallet.spent
+			//let budget = entry.wallet.budget
+			
+			//quick error check
+			if(spent == -1.00){
+				//error pulling the data
+				Text("There was a problem pulling your data from the app.")
+					.frame(maxWidth: .infinity, alignment: .center)
+					.font(.footnote)
+			}else{
+				//successfully pulled down the data
+				
+				//display either large or small version of the gauge
+				switch family {
+				case .systemSmall:
+					SmallWalletGaugeView(wallet: entry.wallet)
+				case .systemMedium:
+					WalletGaugeView(wallet: entry.wallet)
+						.frame(width: 340, height: 175)
+				default:
+					SmallWalletGaugeView(wallet: entry.wallet)
+				}
+			}
         }
     }
 }
@@ -74,7 +98,10 @@ struct WalletStatusWidget: Widget {
         StaticConfiguration(kind: kind, provider: StatusProvider()) { entry in
             if #available(iOS 17.0, *) {
                 StatusEntryView(entry: entry)
+					.frame(maxWidth: .infinity, maxHeight:.infinity)
                     .containerBackground(.fill.tertiary, for: .widget)
+					.background(Color.accentColor.gradient)
+					.clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
             } else {
                 StatusEntryView(entry: entry)
                     .padding()
@@ -83,6 +110,7 @@ struct WalletStatusWidget: Widget {
         }
         .configurationDisplayName("Wallet Status")
         .description("Displays your current wallet status.")
+		.supportedFamilies([.systemSmall, .systemMedium])
     }
 }
 
